8afc72a5838b717f0f5c56a75d2d494783e9c111
lexus2727 <lexus2424@yahoo.com>
2020-03-15 19:53:37 -0400

    update

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,7 @@ const About = () => {
         <div class="row firstRow">
           <section>
 			<h2>About Us</h2>
-			<p>Event Planner Ltd is an event logistics and marketing company which was formed back in 2013.
-               Our company offers A-Z event planning services by a team of experienced and energetic event
-               planners, suppliers, venues and more. One of the main reasons behind the success of Event 
-               Planner Ltd is the fact that<strong> we do not charge our clients fees! </strong>
-               Due to the numerous events that we organize, Event Planner Ltd does not need to add exorbitant fees and mark-ups to make 
-               profit margins. This ensures that our clients list, which is constantly growing, make regular use of
-               our services.</p>
+			<p>React Event Planner allows you to create and plan your next event for free!</p>
 		  </section>
         </div>
     )
